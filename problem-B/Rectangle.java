26c29f8b377d82499a5cd40320a66eddfabefdc2
zhaniyazhaniya <124429520+zhaniyazhaniya@users.noreply.github.com>
2023-02-11 02:56:25 +0600

    changed private attributes to protected

--- problem-B/Rectangle.java
+++ problem-B/Rectangle.java
@@ -1,6 +1,6 @@
 public class Rectangle extends Shape {
-    private double width;
-    private double length;
+    protected double width;
+    protected double length;
 
     public Rectangle() {
         this.width = 1.0;
