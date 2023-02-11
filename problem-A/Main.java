ceaa427dfe56f08fb3794c80bbb70e0e7ecbfbc5
zhaniyazhaniya <124429520+zhaniyazhaniya@users.noreply.github.com>
2023-02-11 12:54:36 +0600

    Update Main.java

---
+++ problem-A/Main.java
@@ -0,0 +1,12 @@
+public class Animal {
+    public static void main (String[] args){
+        Animal animal = new Animal("Tiger");
+        System.out.println(animal);
+        Mammal mammal = new Mammal("Wolf");
+        System.out.println(mammal);
+        Dog dog = new Dog("Copper");
+        System.out.println(dog);
+        Cat cat = new Cat("Coco");
+        System.out.println(cat);
+    }
+}
